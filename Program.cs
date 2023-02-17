3dc4895a1bb5e7e118f71d3171aaf22027341591
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-17 13:30:38 +0530

    Check if Two lines are Equal or not

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace LineComaprisonWithOops
+﻿using LineComparisonWithOops;
+
+namespace LineComaprisonWithOops
 {
     public class LineComparison
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Program");
+            CheckEquality check = new CheckEquality();
+            check.CheckLength();
         }
     }
 }
