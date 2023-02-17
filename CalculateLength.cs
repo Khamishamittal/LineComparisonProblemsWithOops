4e1f7d640f9ef7f3d35328df43ec445f0c403718
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-17 13:23:53 +0530

    Calculate length of a line using coordinates x and y

---
+++ CalculateLength.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonWithOops
+{
+    public class CalculateLength
+    {
+        public void Calculate()
+        {
+            double x1, y1, x2, y2;
+            double length;
+
+            Console.WriteLine("Enter coordinates of start point x1 & y1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter coordinates of end point x2 & y2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Lenght of a Line is : " + length + " cm");
+
+        }
+    }
+}
+    
+
